9367caeeb073b2aa0b4c58343a3fb8eb860be1b3
Jeff Linton <jslinton44@gmail.com>
2016-01-30 13:36:11 -0700

    Upgrades to .net 4.6.1

--- PriorityQueue/Program.cs
+++ PriorityQueue/Program.cs
@@ -1,10 +1,5 @@
 ﻿using PriorityQueue.Collections;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PriorityQueue
 {
